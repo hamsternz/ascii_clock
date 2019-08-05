a638563456f1aff3a85fe430f68455f40f61a823
Mike Field <hamster@snap.net.nz>
2019-08-05 23:22:25 +1200

    Add some colour

--- ascii_clock.c
+++ ascii_clock.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
 
@@ -15,9 +16,8 @@ const char patterns[8][88] =  {
    "| *_/ |  _| |_   /  /_   ___| |     | |  ___| | | |_| |  /  /   | |_| |     | |   |_|   ",
    "*_____/ |_____| |_____| |_____/     |_| |_____/ *_____/ |__/    *_____/     |_|         "};
 
-const char frame[10][68] = {
+const char frame[9][68] = {
     "+-----------------------------------------------------------------+",
-    "|                                                                 |",
     "|                                                                 |",
     "|                                                                 |",
     "|                                                                 |",
@@ -34,6 +34,14 @@ void set_colour_RGB(int r, int g, int b) {
   printf("\x1b[38;5;%im",16+r*36+g*6+b);
 }
 
+void set_position(int y, int x) {
+  printf("\x1b[%i;%iH",y,x);
+}
+
+void clear(void) {
+  printf("\x1b[2J");
+}
+
 int main(int argc, char *argv[]) {
   int line,c,i;
   int  digits[8] = {0,1,10,3,4,10,6,7};
@@ -41,40 +49,44 @@ int main(int argc, char *argv[]) {
   struct tm *now_tm;
   int hrs, mins, secs;
 
-  now = time(NULL);
-  now_tm = localtime(&now);
-  hrs  = now_tm->tm_hour;
-  mins = now_tm->tm_min;
-  secs = now_tm->tm_sec;
-
-  digits[0] = hrs/10;
-  digits[1] = hrs%10;
-
-  digits[3] = mins/10;
-  digits[4] = mins%10;
-
-  digits[6] = secs/10;
-  digits[7] = secs%10;
-
+  clear();
   printf("%s",clr_red);
-  printf("%s\n",frame[0]);
-  for(line = 0; line < 6; line++) {
-  
-    putchar(frame[line+1][0]);
-    putchar(frame[line+1][1]);
-    printf("%s",clr_green);
-    for(c = 0; c < 8; c++) {
-       for(i = 0; i < 8; i++) {
-	  char a = patterns[line][digits[c]*8+i];
-	  if(a == '*') a = '\\';
-	  putchar(a);
-       }
-    }
-    printf("%s",clr_red);
-    putchar(frame[line+1][2+64]);
-    putchar('\n');
+  for(line = 0; line < 9; line++) {
+    set_position(2+line, 2);
+    printf("%s\n",frame[line]);
   }
-  printf("%s\n",frame[8]);
-  printf("%s\n",frame[9]);
-  printf("%s\n",clr_white);
+ 
+  while(1) {
+    now = time(NULL);
+    now_tm = localtime(&now);
+    hrs  = now_tm->tm_hour;
+    mins = now_tm->tm_min;
+    secs = now_tm->tm_sec;
+
+    digits[0] = hrs/10;
+    digits[1] = hrs%10;
+
+    digits[3] = mins/10;
+    digits[4] = mins%10;
+
+    digits[6] = secs/10;
+    digits[7] = secs%10;
+
+    printf("%s",clr_green);
+    for(line = 0; line < 6; line++) {
+      set_position(3+line,4);
+      for(c = 0; c < 8; c++) {
+         for(i = 0; i < 8; i++) {
+	    char a = patterns[line][digits[c]*8+i];
+	    if(a == '*') a = '\\';
+	    putchar(a);
+         }
+      }
+    }
+    fflush(stdout);
+    set_position(5+line,3);
+    printf("%s\n",clr_white);
+    sleep(1);
+  }
+
 }
