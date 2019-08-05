99d48630831dc27965e0049ed0afec552cf12426
Mike Field <hamster@snap.net.nz>
2019-08-05 22:29:15 +1200

    Added some colour

--- ascii_clock.c
+++ ascii_clock.c
@@ -2,15 +2,37 @@
 #include <time.h>
 #include <sys/time.h>
 
+const char clr_red[]   = "\x1b[1;31m";
+const char clr_green[] = "\x1b[1;32m";
+const char clr_white[] = "\x1b[1;37m";
 
-char patterns[8][88] =  {
+
+const char patterns[8][88] =  {
    " _____    __     _____   _____   _   _   _____   _____   _____   _____   _____          ",
-   "/  _  *  /  |   /     | |___  * | | | | |  ___| /  ___| |___  | /  _  * /  _  *    _    ",
+   "/  _  *  /  |   /  _  | |___  * | | | | |  ___| /  ___| |___  | /  _  * /  _  *    _    ",
    "| / * | |_  |   |_//  /   __| | | |_| | | |___  | |___     /  | | |_| | | |_| |   |_|   ",
    "| | | |   | |     /  /   |__  | |___  | |___  * |  _  *   /  /  |  _  | *___  |    _    ",
    "| *_/ |  _| |_   /  /_   ___| |     | |  ___| | | |_| |  /  /   | |_| |     | |   |_|   ",
    "*_____/ |_____| |_____| |_____/     |_| |_____/ *_____/ |__/    *_____/     |_|         "};
 
+const char frame[10][68] = {
+    "+-----------------------------------------------------------------+",
+    "|                                                                 |",
+    "|                                                                 |",
+    "|                                                                 |",
+    "|                                                                 |",
+    "|                                                                 |",
+    "|                                                                 |",
+    "|                                                                 |",
+    "|                                                                 |",
+    "+-----------------------------------------------------------------+"};
+
+void set_colour_RGB(int r, int g, int b) {
+  r %= 6;
+  g %= 6;
+  b %= 6;
+  printf("\x1b[38;5;%im",16+r*36+g*6+b);
+}
 
 int main(int argc, char *argv[]) {
   int line,c,i;
@@ -34,8 +56,13 @@ int main(int argc, char *argv[]) {
   digits[6] = secs/10;
   digits[7] = secs%10;
 
+  printf("%s",clr_red);
+  printf("%s\n",frame[0]);
   for(line = 0; line < 6; line++) {
-    putchar(' ');
+  
+    putchar(frame[line+1][0]);
+    putchar(frame[line+1][1]);
+    printf("%s",clr_green);
     for(c = 0; c < 8; c++) {
        for(i = 0; i < 8; i++) {
 	  char a = patterns[line][digits[c]*8+i];
@@ -43,7 +70,11 @@ int main(int argc, char *argv[]) {
 	  putchar(a);
        }
     }
+    printf("%s",clr_red);
+    putchar(frame[line+1][2+64]);
     putchar('\n');
   }
-  putchar('\n');
+  printf("%s\n",frame[8]);
+  printf("%s\n",frame[9]);
+  printf("%s\n",clr_white);
 }
